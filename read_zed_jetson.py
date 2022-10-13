52eac777f72093c9206b9b3b6a053af4486ba8d8
conghung43@gmail.com <conghung43@gmail.com>
2022-10-13 15:40:46 +0800

    0.10.13

---
+++ read_zed_jetson.py
@@ -0,0 +1,30 @@
+
+import cv2
+import numpy as np
+import pyzed.sl as sl
+import time
+init = sl.InitParameters()
+init.camera_resolution = sl.RESOLUTION.HD2K
+init.depth_mode = sl.DEPTH_MODE.NONE
+cam = sl.Camera()
+if not cam.is_opened():
+    print("Opening ZED Camera...")
+status = cam.open(init)
+if status != sl.ERROR_CODE.SUCCESS:
+    print(repr(status))
+    exit()
+runtime = sl.RuntimeParameters()
+mat = sl.Mat()
+while True:
+    # ret, frame = cap.read()
+    err = cam.grab(runtime)
+    if err == sl.ERROR_CODE.SUCCESS:
+        #if time.localtime(time.time()).tm_sec % 2 ==0:
+        #    continue
+        cam.retrieve_image(mat, sl.VIEW.LEFT)
+        image = mat.get_data()
+        image = cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+        cv2.imshow('Image', image)
+        code = cv2.waitKey(10)
+        if code == ord('q'):
+            break
