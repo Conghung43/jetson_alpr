e57283d0081c0b292cc1c404df66d7e5078bf3f6
conghung43@gmail.com <conghung43@gmail.com>
2022-10-15 16:17:17 +0800

    0.10.15 update sqlite3

--- test_sqlite.py
+++ test_sqlite.py
@@ -1,7 +1,7 @@
 import sqlite3
 import random
 import string
-import time
+from datetime import datetime
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,30 +9,45 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def insert_plate(db, cursor, plate_number):
     cursor.execute(
     """INSERT INTO PLATE(PLATE_NO) VALUES (?)""",
-    (plate_number))
+    [plate_number],)
     db.commit()
 
 def insert_related_plate_data(db, cursor, plate_informations):
     cursor.execute(
-    """INSERT INTO PLATE_INFO(TIME,LATITUDE,LONGTITUDE) VALUES (?,?,?)""", plate_informations)
+    """INSERT INTO PLATE_INFORMATION(RECORDED_TIME,LATITUDE,LONGTITUDE, PLATE_ID) VALUES (?,?,?,?)""", (plate_informations))
     db.commit()
 
 def generate_plate_information():
     plate_number = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + random.choice(string.digits) + random.choice(string.digits) + random.choice(string.digits) + random.choice(string.digits)
-    recorded_time = time.time()
+    recorded_time = datetime.now().strftime("%B %d, %Y %I:%M%p")
     longtitude = random.uniform(22,23)
     latitude = random.uniform(120,121)
     return [plate_number, recorded_time, longtitude, latitude]
 def main():
-    db_path = os.path.join(BASE_DIR, "alpr_jetson.db")
+    # db_path = os.path.join(BASE_DIR, "alpr_jetson.db")
     # with sqlite3.connect(db_path) as db:
     db = sqlite3.connect("main.db")
     cursor = db.cursor()
     plate_information = generate_plate_information()
     # cursor.execute("CREATE TABLE PLATE (ID INTEGER PRIMARY KEY AUTOINCREMENT,  PLATE_NO VARCHAR(10))")
-    cursor.execute("SELECT * FROM PLATE")
+    # cursor.execute("""CREATE TABLE PLATE_INFORMATION (ID INTEGER PRIMARY KEY AUTOINCREMENT,  
+    #                                         RECORDED_TIME VARCHAR(255),
+    #                                         LATITUDE VARCHAR(255),
+    #                                         LONGTITUDE VARCHAR(255),
+    #                                         PLATE_ID INTEGER,
+    #                                         FOREIGN KEY(PLATE_ID) REFERENCES PLATE(ID))""")
+    # cursor.execute("ALTER TABLE PLATE_INFORMATION ADD TIME DATETIME")
+    # insert_plate(db, cursor, plate_information[0])
+    # insert_plate(db, cursor, plate_information[1:].append(0))
+    # cursor.execute("DROP TABLE PLATE_INFORMATION")
+    # cursor.execute("DELETE FROM PLATE WHERE id=?", (4,))
+
+    plate_information[1:].append(2)
+    insert_related_plate_data(db, cursor, plate_information)
+    cursor.execute("SELECT * FROM PLATE_INFORMATION")
     user_all = cursor.fetchall()
-    print('')
+    print(user_all)
+    # db.commit()
 
 main()
 
